78bb6757126c824372babffef63fe36a7a7e8d7e
Ubuntu <ubuntu@ip-172-31-95-119.ec2.internal>
2021-11-29 18:18:02 +0000

    Fixed Headers using SwiftLint

--- microwave.swift
+++ microwave.swift
@@ -1,9 +1,12 @@
 /*
- * Outputs the total time required to reheat a specific food (sub, pizza, or soup)
+  microwave.swift
+
+  By Zefeng Wang
+  Created on November 26, 2021
+
+  Outputs the total time required to reheat a specific food (sub, pizza, or soup)
  given the times for each and the quantity (maximum 3). 
- * By Zefeng Wang
- * Created on November 26, 2021
- * */
+*/
 
 print("What item do you want (sub, pizza, or soup)?")
 var total = 0
@@ -12,12 +15,12 @@ print("How many items (max 3)?")
 let count = Int(readLine()!)!
 let increaseFactor = (count != 1 ? (count == 2 ? 1.5 : 2) : 1)
 switch item {
-  case "sub":
-    total += Int(60 * increaseFactor)
-  case "pizza":
-    total += Int(45 * increaseFactor)
-  default:
-    total += Int(105 * increaseFactor)
+case "sub":
+  total += Int(60 * increaseFactor)
+case "pizza":
+  total += Int(45 * increaseFactor)
+default:
+  total += Int(105 * increaseFactor)
 }
 
 let min = Int(total / 60)
